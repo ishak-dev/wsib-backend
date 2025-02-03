1c2a547c2dfed6cfd54b513bd80349fee8010969
ishak-dev <ishak.kazic@gmail.com>
2025-02-03 23:31:49 +0100

    first commit

---
+++ src/main/java/com/what_should_i_buy/whatShouldIBuyApp/WhatShouldIBuyAppApplication.java
@@ -0,0 +1,13 @@
+package com.what_should_i_buy.whatShouldIBuyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIBuyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatShouldIBuyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/what_should_i_buy/whatShouldIBuyApp/WhatShouldIBuyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.what_should_i_buy.whatShouldIBuyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldIBuyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
